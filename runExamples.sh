5b4d823e73b9611a1877446f347889f04632fa01
aonamrata <aonamrata@gmail.com>
2015-11-29 15:24:03 +0530

    Fixed shell and op to have exact result match

--- runExamples.sh
+++ runExamples.sh
@@ -2,9 +2,19 @@
 
 for filename in examples/*.txt; do
 	arguments=$( echo $filename | grep .txt | cut -d'.' -f1 | sed "s/^examples\///")
-    cmd="$(echo php scripts/fileParser.php items.csv $arguments)"
-    printf "Output of: $cmd is: \n\n"
-	OUTPUT="$($cmd)"
-	echo "${OUTPUT}"
+	cmd="$(echo php scripts/fileParser.php items.csv $arguments)"
 	printf "\n------------------------------------------\n"
+	printf "Matching Output of: $cmd with autual run resul..\n\n"
+	OUTPUT="$($cmd)"
+	echo "${OUTPUT}" > "examples/myop2"
+
+	if  diff "${filename}" "examples/myop2" | grep -q ">"  
+	then
+		echo "DONT MATCH"
+	else
+		echo "MATCH"
+		
+	fi
+	
+	# exit
 done
--- src/Service/OutputWriters/StdOutputWriter.php
+++ src/Service/OutputWriters/StdOutputWriter.php
@@ -11,19 +11,20 @@ class StdOutputWriter implements IWriter {
 
     public function writeResult($data) {
         if (empty($data)) {
-            echo PHP_EOL . "No results found..";
+            echo "No results found.\n";
             return;
         }
         //not static as some other writer might need constructor for init of connection configs
-        echo PHP_EOL;
-        foreach ($data as $eachRecord) {
-            echo "{$eachRecord[0]} {$eachRecord[1]} ({$eachRecord[2]})" . PHP_EOL;
+        foreach ($data as $key => $eachRecord) {
+            echo "{$eachRecord[0]} {$eachRecord[1]} ({$eachRecord[2]})\n";
             if (count($eachRecord[3]) > 0) {
-                echo "- " . (implode(PHP_EOL . "- ", $eachRecord[3])) . PHP_EOL;
+                echo "- " . (implode("\n- ", $eachRecord[3])) . "\n";
             }
-            echo PHP_EOL;
+            
+            if($key < count($data)-1) {
+                echo "\n";
+            }
         }
-        echo PHP_EOL;
     }
 
 }
